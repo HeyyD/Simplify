c91316e7c8fe5ee5816437ba631997fe3211006f
HeyyD <heidi.hati@cs.tamk.fi>
2018-03-06 13:10:31 +0200

    Implement interfaces
    -NotificationCallback
    -ConnectionStateCallback

--- app/src/main/java/com/simplify/hmhat/simplify/MainActivity.java
+++ app/src/main/java/com/simplify/hmhat/simplify/MainActivity.java
@@ -3,11 +3,53 @@ package com.simplify.hmhat.simplify;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Error;
+import com.spotify.sdk.android.player.PlayerEvent;
+import com.spotify.sdk.android.player.SpotifyPlayer;
+
+public class MainActivity extends AppCompatActivity implements
+        SpotifyPlayer.NotificationCallback, ConnectionStateCallback
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onLoggedIn() {
+        
+    }
+
+    @Override
+    public void onLoggedOut() {
+
+    }
+
+    @Override
+    public void onLoginFailed(int i) {
+
+    }
+
+    @Override
+    public void onTemporaryError() {
+
+    }
+
+    @Override
+    public void onConnectionMessage(String s) {
+
+    }
+
+    @Override
+    public void onPlaybackEvent(PlayerEvent playerEvent) {
+
+    }
+
+    @Override
+    public void onPlaybackError(Error error) {
+
     }
 }
