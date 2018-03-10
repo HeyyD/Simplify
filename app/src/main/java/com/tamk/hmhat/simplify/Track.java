c05bfc02cb35a82d931ec0185621102c329278c8
HeyyD <heidi.hati@cs.tamk.fi>
2018-03-10 13:33:05 +0200

    Track implements Parceable

--- app/src/main/java/com/tamk/hmhat/simplify/Track.java
+++ app/src/main/java/com/tamk/hmhat/simplify/Track.java
@@ -1,10 +1,25 @@
 package com.tamk.hmhat.simplify;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by hmhat on 9.3.2018.
  */
 
-public class Track {
+public class Track implements Parcelable{
+    
+    public static final Creator<Track> CREATOR = new Creator<Track>() {
+        @Override
+        public Track createFromParcel(Parcel in) {
+            return new Track(in);
+        }
+
+        @Override
+        public Track[] newArray(int size) {
+            return new Track[size];
+        }
+    };
 
     private String uri;
     private String name;
@@ -12,6 +27,11 @@ public class Track {
     public Track(String uri, String name) {
         this.uri = uri;
         this.name = name;
+    }
+
+    protected Track(Parcel in) {
+        uri = in.readString();
+        name = in.readString();
     }
 
     public String getUri() {
@@ -23,8 +43,18 @@ public class Track {
     }
 
     @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(uri);
+        parcel.writeString(name);
+    }
+
+    @Override
     public String toString() {
         return this.name;
     }
-
 }
