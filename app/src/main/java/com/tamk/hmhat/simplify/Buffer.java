7917b1388da28efa54c0f304cb6bcb9d5adcba03
HeyyD <heidi.hati@cs.tamk.fi>
2018-03-14 16:42:40 +0200

    buffer animation

--- app/src/main/java/com/tamk/hmhat/simplify/Buffer.java
+++ app/src/main/java/com/tamk/hmhat/simplify/Buffer.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 /**
  * Created by hmhat on 13.3.2018.
@@ -13,6 +15,12 @@ import android.view.ViewGroup;
 public class Buffer extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.buffer, container, false);
+
+        View v = inflater.inflate(R.layout.buffer, container, false);
+
+        Animation rotation = AnimationUtils.loadAnimation(getContext(), R.anim.buffer);
+        v.findViewById(R.id.buffer).setAnimation(rotation);
+
+        return v;
     }
 }
